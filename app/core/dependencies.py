503dff9deb6d0e7a13834bccadfbeed3326ab33e
Alexandra Bogdanova <alexandra.kaichev@gmail.com>
2025-02-27 16:11:03 +0100

    05 Handle admin authorisation

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -9,7 +9,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.security import decode_access_token
 from app.db.database import get_db
 from app.db.models.user import User
+from app.db.models.action import Action
 from app.db.crud.user import get_user_by_username
+from app.db.crud.action import get_action
 from app.schemas.auth import TokenData
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
@@ -42,3 +44,62 @@ async def get_current_user(
         )
 
     return user
+
+async def get_current_admin_user(
+    current_user: User = Depends(get_current_user)
+) -> User:
+    """
+    Retrieve current admin user or throw an exception
+
+    Args:
+        current_user (User): Currently authenticated user
+
+    Returns:
+        User: Currently authenticated admin user
+
+    Raises:
+        HTTPException: Raises 403 HTTP error when not admin user
+    """
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Admin access required"
+        )
+
+    return current_user
+
+async def get_current_user_authorised_by_action(
+    action_id: int,
+    current_user: User = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db)
+) -> User:
+    """
+    Ensures that user is either admin or owner of the action
+    Returns the action if operation on the action can be performed
+
+    Args:
+        action_id (int): ID of the action to manage
+        current_user (User): currently connected user
+        db (AsyncSession): Database async session
+
+    Returns:
+        User: Returns connected user authorised to manage action
+
+    Raises:
+        HTTPException:
+            - 404 if action does not exist
+            - 403 if user is not authorised to edit action
+    """
+    action: Action = await get_action(db, action_id)
+    if not action:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND
+        )
+
+    if action.user_id != current_user.id and not current_user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorised to modify this action"
+        )
+
+    return current_user
